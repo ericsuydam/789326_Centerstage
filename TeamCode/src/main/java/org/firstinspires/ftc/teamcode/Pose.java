b93cd53eca80e88afd6eab7db41a20d7b833b233
Eric Suydam <eric.suydam@gmail.com>
2023-08-27 11:12:57 -0400

    Update Pose.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pose.java
@@ -1,3 +1,20 @@
 public class Pose {
+  /**
+  * Class to wrap robot Pose
+  *
+  */
+
+  private double x_location;
+  private double y_location;
+  private double heading;
+
+  /**
+  * Init with no initial Pose information
+  */
+  public void Pose() {
+    
+  }
+  
+
   
 }
